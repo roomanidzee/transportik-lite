4973fcdbe020e618fed62ae634c167a769136e51
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-24 10:04:50 +0300

    fixed config

--- server/settings/environments/development.py
+++ server/settings/environments/development.py
@@ -5,4 +5,4 @@ from server.settings.components import config
 DEBUG = True
 
 ALLOWED_HOSTS = [config['domain'], 'localhost', '127.0.0.1', '[::1]']
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'modules.users.User'
--- tests/test_server/test_modules/test_transports/test_analysis/test_cassandra_load.py
+++ tests/test_server/test_modules/test_transports/test_analysis/test_cassandra_load.py
@@ -1,11 +1,17 @@
 import random
 from datetime import datetime
 
+import pytest
+
 from server.modules.transports.models import TransportAnalysis
 from server.modules.transports.repositories.cassandra import CassandraAnalysisRepository
 
 
 def test_load(test_settings):
+
+    if test_settings['cassandra']['contact_points'][0] == 'none':
+        pytest.skip('no connection to test cassandra instance')
+
     cassandra_repo = CassandraAnalysisRepository(test_settings)
 
     for _ in range(1_000_000):
