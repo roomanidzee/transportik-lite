d0dcfd24a1565c5969989a8a632e2a2bc8041aaa
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-12-14 10:39:43 +0300

    grafana, part 1

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -11,8 +11,13 @@ until postgres_ready; do
   >&2 echo 'Postgres is unavailable - sleeping'
 done
 
+cmd="$*"
+
 if [ "$1" = 'start-dev' ]; then
     python manage.py makemigrations;
+    python manage.py makemigrations users;
+    python manage.py makemigrations trips;
+    python manage.py makemigrations transports;
     python manage.py migrate;
     python manage.py collectstatic;
     python manage.py runserver 0.0.0.0:8000
@@ -20,4 +25,7 @@ elif [ "$1" = 'start-prod' ]; then
     sh /code/docker/django/gunicorn.sh
 elif [ "$1" = 'start-celery' ]; then
     celery -E -A server.settings worker -B --scheduler django_celery_beat.schedulers:DatabaseScheduler --loglevel=info
+else
+  exec "$cmd"
 fi
+
--- docker/rabbitmq/entrypoint.sh
+++ docker/rabbitmq/entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+cmd="$*"
+
 if [ "$1" = 'init' ]; then
   sh ./root-init.sh
 elif [ "$1" = 'connect-cluster' ]; then
   sh ./cluster-connect.sh
+else
+  exec "$cmd"
 fi
